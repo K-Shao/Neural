7c748c697833e6cbf574b62b736b80def2e6611c
Kevin Shao <knshao@gmail.com>
2016-09-06 22:06:01 -0400

    Added a VERY simple interface

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,37 +1,72 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class Main {
+	
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static MNIST mnist;
 
 	public static void main(String[] args) throws IOException {
 
-		MNIST mnist = new MNIST ();
-//		Network net = new Network (new int [] {784, 100, 30, 10}, 3, 10);
-//		net.SGD(mnist.getTrainingCases(), 10, mnist);
 		
+		mnist = new MNIST();
 		
-//		Wiki wiki = new Wiki();
-//		Network net = new Network (new int [] {1410, 150, 100, 94}, 2, 30, Calculator.stringTo2DArray(wiki.pullBiases()), Calculator.stringTo3DArray(wiki.pullWeights()));
-//		wiki.test(net); 
-//		for (int i = 0; i < 1000; i++) {
-//			List<TrainingCase> training = wiki.getTrainingCases ();
-//			net.SGD(training, 2, wiki);
-//			wiki.writeBiases(net);
-//			wiki.writeWeights(net);
-//			System.out.println("Training set " + i + " complete.");
-//		}
-
-
-		
-		
+		System.out.println("Welcome to my simple MNIST neural network!");
+		loop();
 		
 
-		
-		Network net = new Network (new int [] {784, 50, 10}, Calculator.stringTo2DArray(mnist.pullBiases()), Calculator.stringTo3DArray(mnist.pullWeights()));
-		mnist.test(net);
 	}
 	
+	public static void loop () {
+		System.out.println("Options: \"train\" to train a new neural network and write out its parameters, " + 
+								"\"test\" to test the saved neural network, and \"exit\" to exit the program. ");
+		String input = null;
+		try {
+			input = br.readLine();
+		} catch (IOException e) {
+			System.out.println("There was an error reading input");
+			System.exit(1);
+		}
+		input = input.trim();
+		input = input.toLowerCase();
+		if (input.equals("train")) {
+			Network net = new Network (new int [] {784, 50, 10});
+			List<TrainingCase> training = mnist.getTrainingCases();
+			net.SGD(training, 10, mnist);
+			try {
+				mnist.writeParams(net);
+			} catch (FileNotFoundException e) {
+				System.out.println("There was an error saving the network. ");
+			}
+			System.out.println("Training complete.");
+		} else if (input.equals("test")) {
+			Network net;
+			try {
+				net = new Network (new int[] {784, 50, 10}, Calculator.stringTo2DArray(mnist.pullBiases()), Calculator.stringTo3DArray(mnist.pullWeights()));
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Failed to load parameters. Make sure that ./Resources/mnist_biases.txt and ./Resources/mnist_weights.txt are both present and untampered. ");
+				loop();
+				return;
+			}
+			mnist.test(net);
+			System.out.println("Testing complete");
+		} else if (input.equals("exit")) {
+			System.out.println("Bye!");
+			System.exit(0);
+		} else {
+			System.out.println("Sorry, your input doesn't seem to match one of the options. Please try again. ");
+		}
+		
+		
+		
+		loop();
+	}
 
 	
 
